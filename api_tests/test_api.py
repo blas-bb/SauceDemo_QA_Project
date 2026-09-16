e91d6cdb139d638c7f179fdff2a3f7c1ad997bc9
blas-bb <Sladjan3003@Gmail.com>
2026-09-16 22:17:18 +0200

    Add JSON data check
    Added assertion to check the length of user data.

--- api_tests/test_api.py
+++ api_tests/test_api.py
@@ -5,3 +5,6 @@ def test_get_users():
     response = requests.get("https://jsonplaceholder.typicode.com/users")
 
     assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 10
